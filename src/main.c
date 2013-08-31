f340ae06e5dd8a008d56817daec72e58957b5ceb
John Fulton <jf.1987@gmail.com>
2013-08-31 22:53:34 +1000

    Replaced buggy hash table library. Now getting floating point errors.

--- src/main.c
+++ src/main.c
@@ -3,9 +3,9 @@
 #include <omp.h>
 #include <string.h>
 #include <time.h>
-#include <cfuhash.h>
+#include <search.h>
 
-#define INITIAL_HASH_SIZE 100
+#define HASH_TABLE_SIZE 500000
 #define MAX_WORD_LENGTH 40
 #define MAX_FILE_LENGTH 100
 #define PATH_TO_DATA "../data/"
@@ -27,7 +27,12 @@ char *inputs[INPUTS_COUNT];
 
 int *start_pos;
 
-cfuhash_table_t *shakespeare_hash;
+//cfuhash_table_t *shakespeare_hash;
+
+typedef struct {
+	int count;
+	omp_lock_t *lock;
+} word_count_t;
 
 
 // prototypes
@@ -36,7 +41,8 @@ void readShakespeare(void);
 void readInputs(void);
 int isAlphaNum(char);
 void lcase(char*);
-void print_hash(cfuhash_table_t*);
+//void print_hash(cfuhash_table_t*);
+void clear_string(char*);
 
 // PARAMS:
 // -t #NUM indicates number of threads to run on
@@ -44,7 +50,7 @@ void print_hash(cfuhash_table_t*);
 // -sf #FILE indicates the path of the shakespeare filez
 
 int main(int argc, char **argv) {
-	printf("Hello world! Args:\n");
+	//printf("Hello world! Args:\n");
 
 	// Assign paths
 	strcpy(shakespearePath, PATH_TO_DATA);
@@ -55,7 +61,7 @@ int main(int argc, char **argv) {
 		for(i = 0; i < INPUTS_COUNT; i++) {
 			strcpy(inputPaths[i], PATH_TO_DATA);
 			strcat(inputPaths[i], input_files[i]);
-			printf("INPUT FILE %d:%s\n", i+1,inputPaths[i]);
+			//printf("INPUT FILE %d:%s\n", i+1,inputPaths[i]);
 		}
 	}
 
@@ -64,6 +70,8 @@ int main(int argc, char **argv) {
 		printf("%s\n", argv[i]);
 	}*/
 
+
+	// Read input files to buffer
 	readShakespeare();
 	readInputs();
 
@@ -91,25 +99,20 @@ int main(int argc, char **argv) {
 		}
 	}
 
+	printf("Divided problem\n");
+
+	// Initialise hash table of words
+
 
 	// DEBUG: Print the things
-	{
+	/*{
 		int i;
 		for(i = 0; i < NUM_THREADS; i++) {
 			printf("start_pos[%d] = %d, Len:%d, char:'%c'\n", i,start_pos[i], start_pos[i+1] - start_pos[i], shakespeare[start_pos[i]]);
 		}
-	}
+	}*/
 
 
-	shakespeare_hash = cfuhash_new_with_initial_size(INITIAL_HASH_SIZE);
-	// CFU hash functions:
-	// cfuhash_put, cfuhash_delete, cfuhash_get, cfuhash_exists, cfuhash_clear, cfuhash_destroy
-	// cfuhash_pretty_print.
-	
-	/*cfuhash_put(hash, "key1", "1");
-	cfuhash_put(hash, "key2", "2");
-	cfuhash_put(hash, "key3", "3");
-	cfuhash_pretty_print(hash, stdout);*/
 
 	// Do word counting in parallel:
 	omp_set_num_threads(NUM_THREADS);
@@ -118,8 +121,10 @@ int main(int argc, char **argv) {
 		int id = omp_get_thread_num();
 		int i = 0, inword = 0;
 		char thisword[MAX_WORD_LENGTH];
-		printf("Hello from thread %d\n", id);
+		//printf("Hello from thread %d\n", id);
+		// TODO does this skip the last character?
 		for(i = start_pos[id]; i < start_pos[id+1]; i++) {
+			printf("'%c'", shakespeare[i]);
 			if(isAlphaNum(shakespeare[i])) {
 				inword = 1;
 				strncat(thisword, &shakespeare[i],1);
@@ -128,41 +133,95 @@ int main(int argc, char **argv) {
 				// If we were in a word, we have 
 				// reached the end.
 				if(inword) {
+					printf("lcase the word '%s'\n", thisword);
 					lcase(thisword);
+					printf("done\n");
 					//printf("Finished word '%s'\n", thisword);
-					if(cfuhash_exists(shakespeare_hash, thisword)) {
-						int *count = (int*)cfuhash_get(shakespeare_hash, thisword);
-						(*count)++;
-						//printf("New value: %s\n", value);
-						cfuhash_put(shakespeare_hash, thisword, count);
+					ENTRY e, *ep;
+					printf("setting key..\n");
+					e.key = thisword;
+					printf("search for word %s", thisword);
+					if(ep = hsearch(e, FIND)) {
+						printf("found\n");
+						// If an entry exists for this word, aquire a lock
+						// then update the count.
+						printf("Incrementing word count...\n");
+						omp_set_lock(((word_count_t*)ep->data)->lock);
+						((word_count_t*)ep->data)->count++;
+						omp_unset_lock(((word_count_t*)ep->data)->lock);
 					} else {
-						int *new = (int*)malloc(sizeof(int));
-						*new = 1;
-						cfuhash_put(shakespeare_hash, thisword, new);
-					}
-					int j;
-					for(j = 0; j < MAX_WORD_LENGTH && thisword[j] != '\0'; j++) {
-						thisword[j] = '\0';
+						printf("Creating new entry...\n");
+						word_count_t *new = malloc(sizeof(word_count_t));
+						omp_lock_t *newlock;
+						omp_init_lock(newlock);
+						new->count = 0;
+						new->lock = newlock;
+
+						e.data = new;
+
+						hsearch(e, ENTER);
 					}
 
+					// Clear out the current word.
+					clear_string(thisword);
 					inword = 0;
 				}
 
 			}
 
-			// Clear word
 			
 		}
 	}
 
 	//cfuhash_pretty_print(shakespeare_hash, stdout);
-	print_hash(shakespeare_hash);
+	//print_hash(shakespeare_hash);
+
+	// Iterate through the input file and print out the word counts7
+	{
+		int i, inword = 0;
+		char thisword[MAX_WORD_LENGTH];
+		for(i = 0; i < MAX_WORD_LENGTH; i++) {
+			thisword[i] = '\0';
+		}
+		for(i = 0; inputs[0][i] != '\0'; i++) {
+			if(isAlphaNum(inputs[0][i])) {
+				inword = 1;
+				strncat(thisword, &inputs[0][i],1);
+			} else {
+				if(inword) {
+					int count;
+					count = 0;
+					lcase(thisword);
+					ENTRY e, *ep;
+					e.key = thisword;
+					if(ep = hsearch(e, FIND)) {
+						count = ((word_count_t*)ep->data)->count;
+					} else {
+						//printf("Getting value for unknown hash..\n");
+						//count = cfuhash_get(shakespeare_hash, thisword);
+					}
+
+					// TODO uncomment this.
+					printf("%s #%d\n", thisword, count);
+					/*for(j = 0; j < MAX_WORD_LENGTH; j++) {
+						printf("%d,", (int)thisword[j]);
+					}
+					printf("\n");*/
+
+				}
+				inword = 0;
+				clear_string(thisword);
+			}
+		}
+	}
+
+	//print_hash(shakespeare_hash);
 
 
 }
 
 int isAlphaNum(char c) {
-	return ((c >= 65 && c <= 90) || (c >= 97 && c <= 122));
+	return ((c >= 65 && c <= 90) || (c >= 97 && c <= 122) || (c >= 48 && c <= 57));
 }
 
 void readArgs(int argc, char **argv) {
@@ -214,7 +273,7 @@ void lcase(char* string) {
 	}
 }
 
-void print_hash(cfuhash_table_t *hash) {
+/*void print_hash(cfuhash_table_t *hash) {
 	char **keys;
 	size_t key_count;
 	size_t *key_sizes;
@@ -226,4 +285,11 @@ void print_hash(cfuhash_table_t *hash) {
 		printf("\t\"%s\" => %d\n", keys[i], *val);
 	}
 	printf("}\n");
+}*/
+
+void clear_string(char* string){
+	int i;
+	for(i = 0; string[i] != '\0'; i++) {
+		string[i] = '\0';
+	}
 }
